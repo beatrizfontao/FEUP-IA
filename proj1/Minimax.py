304a4cf02c5386f52c2733c15c0e9f472bf177f3
Rita <up202004155@up.pt>
2023-03-25 22:24:54 +0000

    minimax

--- proj1/Minimax.py
+++ proj1/Minimax.py
@@ -1,16 +1,20 @@
 from copy import deepcopy
 from Board import *
     
-def execute_minimax_move(evaluate_func, depth):
+def execute_minimax_move(board, evaluate_func, depth):
     best_move = None
     best_eval = float('-inf')
-    for move in game.state.available_moves:
-        new_state = game.state.move(move)
-        new_state_eval = minimax(new_state, depth - 1, float('-inf'), float('inf'),False, game.state.player, evaluate_func)
-        if new_state_eval > best_eval:
-            best_move = new_state
-            best_eval = new_state_eval
-    game.state = best_move
+    for (piece, moves) in board.get_valid_moves(board.turn):
+            for move in moves:
+                copy_board = deepcopy(board)
+                copy_board.move(piece, move[0], move[1])
+                new_state_eval = minimax(copy_board, depth - 1, float('-inf'), float('inf'), False, board.turn, evaluate_func)
+            if new_state_eval > best_eval:
+                best_move = (piece, move)
+                best_eval = new_state_eval
+    
+    (piece, move) = best_move
+    board.move(piece, move[0], move[1])
 
 def minimax(state, depth, alpha, beta, maximizing, player, evaluate_func):
     if depth == 0 or state.is_game_over() != 0:
@@ -18,7 +22,7 @@ def minimax(state, depth, alpha, beta, maximizing, player, evaluate_func):
     
     if maximizing:
         max_eval = float('-inf')
-        for (piece, moves) in state.get_valid_moves():
+        for (piece, moves) in state.get_valid_moves(state.turn):
             for move in moves:
                 copy_state = deepcopy(state)
                 copy_state.move(piece, move[0], move[1])
@@ -29,12 +33,14 @@ def minimax(state, depth, alpha, beta, maximizing, player, evaluate_func):
         return max_eval
     else:
         min_eval = float('inf')
-        for move in state.available_moves:
-            new_state = state.move(move)
-            eval = minimax(new_state, depth - 1, alpha, beta, True, player, evaluate_func)
-            beta = min(min_eval, eval)
-            if alpha <= beta:
-                break
+        for (piece, moves) in state.get_valid_moves(state.turn):
+            for move in moves:
+                copy_state = deepcopy(state)
+                copy_state.move(piece, moves[0], move[1])
+                eval = minimax(copy_state, depth - 1, alpha, beta, True, player, evaluate_func)
+                beta = min(min_eval, eval)
+                if alpha <= beta:
+                    break
         return min_eval
 
 def enemy_min_moves(board, player):
